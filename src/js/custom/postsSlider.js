e3f48238ab245b47e26b33b417bc59202d0420d0
mizza-cz <mizza.cz666@gmail.com>
2026-05-06 09:46:31 +0200

    add business

--- src/js/custom/postsSlider.js
+++ src/js/custom/postsSlider.js
@@ -1,26 +1,30 @@
 document.querySelectorAll(".postsSlider").forEach((slider) => {
   new Swiper(slider, {
-    slidesPerView: 2,
+    slidesPerView: 1,
     spaceBetween: 20,
-    loop: false,
-    allowTouchMove: true,
-    watchOverflow: true,
+    loop: true,
 
     navigation: {
       nextEl: slider.querySelector(".swipe-next"),
       prevEl: slider.querySelector(".swipe-prev"),
     },
+    pagination: {
+      el: slider.querySelector(".swiper-pagination"),
+      clickable: true,
+    },
 
     breakpoints: {
+      540: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
       768: {
         slidesPerView: 3,
         spaceBetween: 24,
-        allowTouchMove: true,
       },
       1240: {
         slidesPerView: 4,
         spaceBetween: 32,
-        allowTouchMove: false,
       },
     },
   });
